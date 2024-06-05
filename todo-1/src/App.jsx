77e3845349d8b997022804ba9f2c6f921dd2d8c6
asep-sang-raja-iblis <saefuddinasep@yahoo.com>
2024-06-05 17:45:52 +0700

    create Todos and TodoItem component

--- todo-1/src/App.jsx
+++ todo-1/src/App.jsx
@@ -1,9 +1,34 @@
-import './App.css'
+import React, { useState } from 'react';
+import Todos from './components/Todos';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Finish Progate React Course',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Have lunch with Guru Domba',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Study React with Ninja Ken',
+      completed: false,
+    },
+  ])
+
+  console.log(todos)
+
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div>
+      <h1>My Todo List</h1>
+      {todos.map((todo) => {
+        return <p key={todo.id}>{todo.title}</p>
+      })}
+      <Todos todos={todos} /> {/* Menampilkan component Todos */}
     </div>
   )
 }
